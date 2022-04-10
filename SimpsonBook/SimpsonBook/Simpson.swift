859d3403a93684b63390697b145673532f2bfc75
Zeliha Uslu <74205513+zelihauslu@users.noreply.github.com>
2022-04-10 22:47:02 +0300

    Add files via upload

---
+++ SimpsonBook/SimpsonBook/Simpson.swift
@@ -0,0 +1,23 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    
+    var name : String
+    var job : String
+    var image : UIImage
+    
+    init(simpsonName: String, simpsonJob:String,simpsonImage:UIImage) {
+        
+        name = simpsonName
+        job = simpsonJob
+        image = simpsonImage
+        
+    }
+    
+}
